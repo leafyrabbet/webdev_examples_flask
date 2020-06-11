01e63093f495bed4b5cd29bbd92fc759d3aac0ac
tommy vegetables <leafyrabbet@gmail.com>
2020-06-11 18:59:33 -0400

    FIX: sed Fixes and Update from previous Commit
    - `sed` usage corrected, original commit failed.
    - Added more printouts and install of flaskapp code files to help make
    things simpler.
    - Didn't want `cp` to run under `sudo` so it's here instead of in the
    `install.sh` script.

--- linux/build.sh
+++ linux/build.sh
@@ -1,8 +1,18 @@
 USERUSER=`id -un`
 USERGROUP=`id -gn`
 UWSGIAPP=`which uwsgi | sed 's;\/;\\\/;g'`
+echo ${USERUSER}
+echo ${USERGROUP}
 echo ${UWSGIAPP}
 
-sed 's/USERUSER/'"$USERUSER"'/g' flaskapp.service > flaskapp_built.service
-sed 's/USERGROUP/'"$USERGROUP"'/g' flaskapp_built.service > flaskapp_built.service
-sed 's/UWSGIAPP/'"$UWSGIAPP"'/g' flaskapp_built.service > flaskapp_built.service
+sed 's/USERUSER/'"$USERUSER"'/g' flaskapp.service |\
+   sed 's/USERGROUP/'"$USERGROUP"'/g' |\
+   sed 's/UWSGIAPP/'"$UWSGIAPP"'/g' \
+   > flaskapp_built.service
+
+cat flaskapp_built.service
+
+mkdir -p /var/www/flaskapp/
+cp ../flask_app.ini /var/www/flaskapp/
+cp ../flask_app.py /var/www/flaskapp/
+cp ../hello.mustache /var/www/flaskapp/
