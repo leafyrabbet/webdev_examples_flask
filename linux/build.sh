e3fe396f639db6d0968c436f044fc5befac48d8d
tommy vegetables <leafyrabbet@gmail.com>
2020-06-11 18:41:59 -0400

    UPD: systemd Build Script Updated
    - Removed copypasted user/group names (invalid) with replacement strings
    - Added replacement strings through `sed` into the build script

--- linux/build.sh
+++ linux/build.sh
@@ -1,4 +1,8 @@
+USERUSER=`id -un`
+USERGROUP=`id -gn`
 UWSGIAPP=`which uwsgi | sed 's;\/;\\\/;g'`
 echo ${UWSGIAPP}
 
-sed 's/UWSGIAPP/'"$UWSGIAPP"'/g' flaskapp.service > flaskapp_built.service
+sed 's/USERUSER/'"$USERUSER"'/g' flaskapp.service > flaskapp_built.service
+sed 's/USERGROUP/'"$USERGROUP"'/g' flaskapp_built.service > flaskapp_built.service
+sed 's/UWSGIAPP/'"$UWSGIAPP"'/g' flaskapp_built.service > flaskapp_built.service
