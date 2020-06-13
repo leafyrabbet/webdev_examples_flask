82e7f7eeea0a0bf521140dca5d8fe3af4f982170
tommy vegetables <leafyrabbet@gmail.com>
2020-06-13 19:08:58 -0400

    UPD: In-Progress update to macOS Install Script
    - Adding stopping condition just in case.
    - Renaming `launchd` service.

--- macos/install.sh
+++ macos/install.sh
@@ -4,15 +4,16 @@ if [[ -n ${ZSH_NAME} ]]; then
    emulate -LR bash;
 fi
 
-INSTALL_DIR=/usr/local/opt/flasktest;
+INSTALL_DIR=/usr/local/opt/flaskapp;
 
 mkdir -p ${INSTALL_DIR};
 cp ../flask_app.ini ${INSTALL_DIR}/flask_app.ini;
 cp ../flask_app.py ${INSTALL_DIR}/flask_app.py;
 cp ../hello.mustache ${INSTALL_DIR}/hello.mustache;
 
-cp com.flasktest.uwsgi.plist ~/Library/LaunchAgents/com.flasktest.uwsgi.plist;
-launchctl unload ~/Library/LaunchAgents/com.flasktest.uwsgi.plist;
-launchctl load ~/Library/LaunchAgents/com.flasktest.uwsgi.plist;
-launchctl stop com.flasktest.uwsgi;
-launchctl start com.flasktest.uwsgi;
+cp com.flasksocket.uwsgi.plist ~/Library/LaunchAgents/com.flasksocket.uwsgi.plist;
+launchctl stop com.flasksocket.uwsgi;
+launchctl unload ~/Library/LaunchAgents/com.flasksocket.uwsgi.plist;
+launchctl load ~/Library/LaunchAgents/com.flasksocket.uwsgi.plist;
+launchctl stop com.flasksocket.uwsgi;
+launchctl start com.flasksocket.uwsgi;
