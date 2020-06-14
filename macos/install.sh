fba3f30692d6942d901a7fe8585212dfc466ced6
tommy vegetables <leafyrabbet@gmail.com>
2020-06-13 21:33:37 -0400

    BRK: Renaming Code References
    - Updating names for code files in install script for macOS.
    - Updating install path to LaunchDaemons as prerequisite to adding a
    `UserName` key-value pair to the `.plist` file.

--- macos/install.sh
+++ macos/install.sh
@@ -4,16 +4,16 @@ if [[ -n ${ZSH_NAME} ]]; then
    emulate -LR bash;
 fi
 
-INSTALL_DIR=/usr/local/opt/flaskapp;
+INSTALL_DIR=/usr/local/opt/flasktest;
 
 mkdir -p ${INSTALL_DIR};
-cp ../flask_app.ini ${INSTALL_DIR}/flask_app.ini;
-cp ../flask_app.py ${INSTALL_DIR}/flask_app.py;
+cp ../flasktest.ini ${INSTALL_DIR}/flasktest.ini;
+cp ../flasktest.py ${INSTALL_DIR}/flasktest.py;
 cp ../hello.mustache ${INSTALL_DIR}/hello.mustache;
 
-cp com.flasksocket.uwsgi.plist ~/Library/LaunchAgents/com.flasksocket.uwsgi.plist;
+cp com.flasksocket.uwsgi.plist /Library/LaunchDaemons/com.flasksocket.uwsgi.plist;
 launchctl stop com.flasksocket.uwsgi;
-launchctl unload ~/Library/LaunchAgents/com.flasksocket.uwsgi.plist;
-launchctl load ~/Library/LaunchAgents/com.flasksocket.uwsgi.plist;
+launchctl unload /Library/LaunchDaemons/com.flasksocket.uwsgi.plist;
+launchctl load /Library/LaunchDaemons/com.flasksocket.uwsgi.plist;
 launchctl stop com.flasksocket.uwsgi;
 launchctl start com.flasksocket.uwsgi;
