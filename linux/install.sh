44288f05f71a1f0ff472c52ab7fa2a40569ac3a3
tommy vegetables <leafyrabbet@gmail.com>
2020-06-13 21:36:00 -0400

    BRK: Moving Files and Renaming with Comments
    - Adding comments to files, in-progress.
    - Moving files and renaming to move towards new naming consistency.

--- linux/install.sh
+++ linux/install.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+# @file
+# @brief ...
+#
+# @author tommy vegetables <leafyrabbet@gmail.com>
+# @copyright 2020, TenonGarden Productions
+
 mv flaskapp_built.service /etc/systemd/system/flaskapp.service
 
 mkdir -p /var/www/flaskapp/
